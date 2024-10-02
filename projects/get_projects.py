aef2d0bc9b941084c0d66135049933b9204a8e08
Alistair Ward <u0991467@redwood5.ipoib.int.chpc.utah.edu>
2024-10-02 13:55:37 -0600

    1. Updated get_projects.py to only output the id (if requested) and only output projects with a defined reference

--- projects/get_projects.py
+++ projects/get_projects.py
@@ -4,9 +4,10 @@ import argparse
 from sys import path
 
 def main():
+  global allowed_references
 
   # Parse the command line
-  args = parseCommandLine()
+  args = parse_command_line()
 
   # Import the api client
   path.append(args.api_client)
@@ -14,18 +15,35 @@ def main():
   api_store = Store(config_file = args.client_config)
   api_mosaic = Mosaic(config_file = args.client_config)
 
+  if args.reference:
+    if args.reference not in allowed_references:
+      fail('Unknown reference genome: ' + str(args.reference))
+
   # Get all the available projects
-  
-  for project in api_mosaic.get_projects(search = args.search):
-    print(project['name'], ': ', project['id'], sep = '')
+  for project_info in api_mosaic.get_projects(search = args.search):
+    display = True
+    if args.reference:
+      if project_info['reference'] != args.reference:
+        display = False
+    if display:
+      if args.output_ids_only:
+        print(project_info['id'], sep = '')
+      else:
+        print(project_info['name'], ': ', project_info['id'], sep = '')
 
 # Input options
-def parseCommandLine():
+def parse_command_line():
   parser = argparse.ArgumentParser(description='Process the command line arguments')
 
   # Define the location of the api_client and the ini config file
   parser.add_argument('--client_config', '-c', required = True, metavar = 'string', help = 'The ini config file for Mosaic')
   parser.add_argument('--api_client', '-a', required = True, metavar = 'string', help = 'The api_client directory')
+
+  # Only output project ids
+  parser.add_argument('--output_ids_only', '-o', required = False, action = 'store_true', help = 'If set, only the project ids will be output')
+
+  # Only output projects of a given reference
+  parser.add_argument('--reference', '-r', required = False, metavar = 'string', help = 'Only output projects with the specified reference')
 
   # Query params
   parser.add_argument('--search', '-s', required = False, metavar = 'string', help = 'Term to search on')
@@ -34,8 +52,12 @@ def parseCommandLine():
 
 # If the script fails, provide an error message and exit
 def fail(message):
-  print(message, sep = "")
+  print(message, sep = '')
   exit(1)
+
+allowed_references = []
+allowed_references.append('GRCh37')
+allowed_references.append('GRCh38')
 
 if __name__ == "__main__":
   main()
