c78a867e9d1635379a09305d39297bf1cdc53023
Alistair Ward <u0991467@redwood5.ipoib.int.chpc.utah.edu>
2024-10-15 13:52:10 -0600

    1. Updated the set_collection_defaults.py script 2. Added a new script to set annotation display types

---
+++ project_setup/set_annotation_defaults.py
@@ -0,0 +1,155 @@
+import os
+import argparse
+import json
+import sys
+
+from os.path import exists
+from pprint import pprint
+from sys import path
+
+def main():
+  global version
+
+  # Parse the command line
+  args = parse_command_line()
+
+  # Import the api client
+  path.append(args.api_client)
+  from mosaic import Mosaic, Project, Store
+  api_store = Store(config_file = args.config)
+  api_mosaic = Mosaic(config_file = args.config)
+  project = api_mosaic.get_project(args.project_id)
+
+  # Get the json file
+  json_filename = get_json_filename(project, args)
+  json_info = read_json_file(json_filename)
+
+  # Get all the public annotations
+  public_annotations = {}
+  update_annotations = {}
+  for annotation in project.get_variant_annotations_to_import():
+    public_annotations[annotation['uid']] = {'id': annotation['id'], \
+      'original_project_id': annotation['original_project_id'], \
+      'type': annotation['value_type']}
+
+    # Make sure each original project has a list in the update_annotations dictionary
+    if annotation['original_project_id'] not in update_annotations:
+      update_annotations[annotation['original_project_id']] = []
+
+  # Get all of the annotations
+  if 'annotations' not in json_info:
+    fail('ERROR: json file does not contain a section titled "annotations" with information on annotations')
+
+  # Loop over all annotations in the json and store information with the original project id of the annotation
+  for annotation in json_info['annotations']:
+    if annotation in public_annotations:
+      category = None
+      display_type = None
+      severity = None
+      value_max_length = None
+      value_truncate_type = None
+
+      annotation_id = public_annotations[annotation]['id']
+      if 'category' in json_info['annotations'][annotation]:
+        category = json_info['annotations'][annotation]['category'] if json_info['annotations'][annotation]['category'] else None
+      if 'severity' in json_info['annotations'][annotation]:
+        severity = json_info['annotations'][annotation]['severity'] if json_info['annotations'][annotation]['severity'] else None
+      if 'display_type' in json_info['annotations'][annotation]:
+        display_type = json_info['annotations'][annotation]['display_type'] if json_info['annotations'][annotation]['display_type'] else None
+      if 'value_max_length' in json_info['annotations'][annotation]:
+        value_max_length = json_info['annotations'][annotation]['value_max_length'] if json_info['annotations'][annotation]['value_max_length'] else None
+      if 'value_truncate_type' in json_info['annotations'][annotation]:
+        value_truncate_type = json_info['annotations'][annotation]['value_truncate_type'] if json_info['annotations'][annotation]['value_truncate_type'] else None
+
+      # Store the information associated with the project id for the annotation
+      original_project_id = public_annotations[annotation]['original_project_id']
+      update_annotations[original_project_id].append({'annotation': annotation, \
+        'annotation_id': annotation_id, \
+        'category': category, \
+        'display_type': display_type, \
+        'severity': severity, \
+        'value_max_length': value_max_length, \
+        'value_truncate_type': value_truncate_type})
+
+  # Purge empty lists from update_annotations
+  for project_id in list(update_annotations.keys()):
+    if len(update_annotations[project_id]) == 0:
+      del(update_annotations[project_id])
+
+  # Loop over all original projects with annotations to update and update them
+  for project_id in update_annotations:
+    project = api_mosaic.get_project(project_id)
+    for annotation in update_annotations[project_id]:
+      annotation_id = annotation['annotation_id']
+      category = annotation['category']
+      display_type = annotation['display_type']
+      severity = annotation['severity']
+      value_max_length = annotation['value_max_length']
+      value_truncate_type = annotation['value_truncate_type']
+      project.put_variant_annotation(annotation_id, display_type = display_type, severity = severity, category = category, value_truncate_type = value_truncate_type, value_max_length = value_max_length)
+
+# Input options
+def parse_command_line():
+  global version
+  parser = argparse.ArgumentParser(description='Process the command line')
+
+  # Required arguments
+  parser.add_argument('--config', '-c', required = True, metavar = 'string', help = 'The config file for Mosaic')
+  parser.add_argument('--api_client', '-a', required = True, metavar = 'string', help = 'The directory where the Python api wrapper lives')
+
+  # Optional pipeline arguments
+  parser.add_argument('--json', '-j', required = False, metavar = 'string', help = 'The json file describing the project defaults')
+  parser.add_argument('--instance', '-i', required = False, metavar = 'string', help = 'The mosaic instance. Will be used to build the json file name')
+  parser.add_argument('--json_path', '-s', required = False, metavar = 'string', help = 'The path to the json file. Not required if --json is set, but will be used to build the json filename')
+
+  # The project id to which the filter is to be added is required
+  parser.add_argument('--project_id', '-p', required = True, metavar = 'integer', help = 'The Mosaic project id to upload attributes to')
+
+  return parser.parse_args()
+
+# Build the json filename
+def get_json_filename(project, args):
+
+  # Build the name of the json file
+  if args.json:
+    json_filename = args.json
+  else:
+    if not args.json_path or not args.instance:
+      fail('No json file was supplied (--json, -j). If no json is supplied, both --json_path (-s) and --instance (-i) must be set so that the filename can be constructed')
+    if not args.json_path.endswith('/'):
+      args.json_path = args.json_path + '/'
+
+    # Get the project reference
+    data = project.get_project_settings()
+    reference = data['reference']
+    json_filename = args.json_path + 'project_defaults_' + str(args.instance) + '_' + str(reference) + '.json'
+
+  return json_filename
+  
+# Check that the json containing the required defaults exists and read in the information
+def read_json_file(json_filename):
+  try:
+    json_file = open(json_filename, 'r')
+  except:
+    fail('Could not open the json file: ' + str(json_filename))
+  try:
+    json_info = json.load(json_file)
+  except:
+    fail('Could not read contents of json file ' + str(json_filename) + '. Check that this is a valid json')
+  json_file.close()
+ 
+  return json_info
+
+# If the script fails, provide an error message and exit
+def fail(message):
+  print(message, sep = '')
+  exit(1)
+
+# Throw a warning
+def warning(message):
+  print('WARNING: ', message, sep = '')
+
+# Initialise global variables
+
+if __name__ == "__main__":
+  main()
--- project_setup/set_collection_defaults.py
+++ project_setup/set_collection_defaults.py
@@ -8,7 +8,6 @@ from pprint import pprint
 from sys import path
 
 def main():
-  global version
 
   # Parse the command line
   args = parseCommandLine()
@@ -33,17 +32,14 @@ def main():
   json_info = read_json_file(json_filename)
 
   # Get all the sample attributes in the project
-  sample_attribute_names = {}
-  sample_attribute_uids = {}
-  sample_attribute_ids = []
+  sample_attributes = {}
   for sample_attribute in project.get_sample_attributes():
+    sample_attributes[sample_attribute['uid']] = {'id': sample_attribute['id'], 'name': sample_attribute['name']}
 
-    # Check for duplicate sample attributes
-    if sample_attribute['name'] in sample_attribute_names:
-      fail('ERROR: there are multiple sample attributes with the name ' + str(sample_attribute['name']) + ' in project with id ' + str(args.project_id))
-    sample_attribute_names[sample_attribute['name']] = sample_attribute['id']
-    sample_attribute_uids[sample_attribute['uid']] = sample_attribute['id']
-    sample_attribute_ids.append(sample_attribute['id'])
+  # Get all the project attributes in the project
+  project_attributes = {}
+  for project_attribute in project.get_project_attributes():
+    project_attributes[project_attribute['uid']] = {'id': project_attribute['id'], 'name': project_attribute['name']}
 
   # Get all the annotations in the project
   annotation_uids = {}
@@ -59,26 +55,57 @@ def main():
   #######
   samples_table_columns = []
   if 'sample_table' in json_info:
-    for attribute_specifier in json_info['sample_table']:
-      attribute_type = json_info['sample_table'][attribute_specifier]
+    for attribute in json_info['sample_table']:
 
-      # If the attribute name was specified
-      if attribute_type == 'name':
-        if attribute_specifier not in sample_attribute_names:
-          fail('ERROR: sample table defaults includes a sample attribute specified by name that is not available: ' + attribute_specifier)
-        samples_table_columns.append(sample_attribute_names[attribute_specifier])
+      # If this is a uid, get and store the id
+      if attribute in sample_attributes:
+        samples_table_columns.append(sample_attributes[attribute]['id'])
 
-      # If the attribute uid was specified
-      elif attribute_type == 'uid':
-        if attribute_specifier not in sample_attribute_uids:
-          fail('ERROR: sample table defaults includes a sample attribute specified by uid that is not available: ' + attribute_specifier)
-        samples_table_columns.append(sample_attribute_uids[attribute_specifier])
+      # If this is not a uid, check if this is the name of a unique sample attribute
+      else:
+        attribute_id = False
+        for sample_attribute in sample_attributes:
+          if attribute == sample_attributes[sample_attribute]['name']:
+            if attribute_id:
+              fail('multiple sample attributes with the name "' + str(attribute) + '" exist in the collection')
+            else:
+              attribute_id = sample_attributes[sample_attribute]['id']
+        if not attribute_id:
+          warning('sample attribute is not available in the selected collection: ' + str(attribute))
+        else:
+          samples_table_columns.append(attribute_id)
 
-      # If the attribute id was specified
-      elif attribute_type == 'id':
-        if attribute_specifier not in sample_attribute_ids:
-          fail('ERROR: sample table defaults includes a sample attribute specified by id that is not available: ' + attribute_specifier)
-        samples_table_columns.append(attribute_specifier)
+  #######
+  #######
+  # Set the projects table defaults
+  #######
+  #######
+  projects_table_columns = []
+  projects_table_attribute_ids = []
+  if 'projects_table' in json_info:
+    allowed_columns = ['NICKNAME', 'PHI_NAME', 'DESCRIPTION', 'ROLE', 'CREATED', 'UPDATED', 'COLLABORATORS', 'REFERENCE', 'VARIANT_COUNT', 'SAMPLE_COUNT', 'ID']
+    for attribute in json_info['projects_table']:
+
+      # If this is one of the "special" attributes, add this to the list
+      if attribute in allowed_columns:
+        projects_table_columns.append(attribute)
+
+      # Warn the user if project attributes in the json are not in the collection and ignore
+      elif attribute in project_attributes:
+        projects_table_columns.append(project_attributes[attribute]['id'])
+      else: 
+        attribute_id = False
+        for project_attribute in project_attributes:
+          if attribute == project_attributes[project_attribute]['name']:
+            if attribute_id:
+              fail('multiple project attributes with the name "' + str(attribute) + '" exist in the collection')
+            else:
+              attribute_id = project_attributes[project_attribute]['id']
+        if not attribute_id:
+          warning('project attribute is not available in the selected collection: ' + str(attribute))
+        else:
+          projects_table_columns.append(attribute_id)
+          projects_table_attribute_ids.append(attribute_id)
 
   #######
   #######
@@ -98,7 +125,9 @@ def main():
     annotation_version_ids = get_variant_table_ids(project, args.project_id, json_info['annotations'], annotation_names, annotation_uids)
 
   # Set the project settings
-  data = project.put_project_settings(selected_sample_attribute_column_ids = samples_table_columns, \
+  data = project.put_collection_project_settings(selected_sample_attribute_column_ids = samples_table_columns, \
+         selected_collections_table_columns = projects_table_columns, \
+         selected_collection_attributes = projects_table_attribute_ids, \
          selected_variant_annotation_version_ids = annotation_version_ids)
   
 # Input options
@@ -117,9 +146,6 @@ def parseCommandLine():
 
   # The project id to which the filter is to be added is required
   parser.add_argument('--project_id', '-p', required = True, metavar = 'integer', help = 'The Mosaic project id to upload attributes to')
-
-  # Version
-  parser.add_argument('--version', '-v', action="version", version='Calypso annotation pipeline version: ' + str(version))
 
   return parser.parse_args()
 
@@ -234,7 +260,7 @@ def get_variant_table_ids(project, project_id, data, annotation_names, annotatio
 
 # If the script fails, provide an error message and exit
 def fail(message):
-  print(message, sep = '')
+  print('ERROR: ', message, sep = '')
   exit(1)
 
 # Throw a warning
@@ -242,9 +268,6 @@ def warning(message):
   print('WARNING: ', message, sep = '')
 
 # Initialise global variables
-
-# Pipeline version
-version = "0.0.1"
 
 if __name__ == "__main__":
   main()
