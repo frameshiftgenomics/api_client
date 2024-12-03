3797091f53d65dc8fe14b60f962bb319c9974907
Alistair Ward <u0991467@redwood5.ipoib.int.chpc.utah.edu>
2024-12-03 11:48:56 -0700

    1. Remove print statement

--- mosaic.py
+++ mosaic.py
@@ -686,7 +686,6 @@ class Project(object):
 
         if emails:
             data['emails'] = emails
-        print(data)
 
         return self._mosaic.post(f'{self._path}/variants/annotations/clinvar/versions/diff', data=data)
 
