3ab450f0590582eaf45496ed0486c48bdcb2c804
Alistair Ward <u0991467@redwood5.ipoib.int.chpc.utah.edu>
2025-03-07 10:16:38 -0700

    1. Started updating routes to check that mosaic is found if api_client is not set

--- project_roles/put_project_role.py
+++ project_roles/put_project_role.py
@@ -1,7 +1,7 @@
-import os
 import argparse
+import json
+import os
 
-from pprint import pprint
 from sys import path
 
 def main():
@@ -18,30 +18,16 @@ def main():
 
   # Import the api client
   path.append(args.api_client)
-  try:
+  try: 
     from mosaic import Mosaic, Project, Store
   except:
     fail('Cannot find mosaic. Please set the --api_client / -a argument')
   api_store = Store(config_file = args.client_config)
   api_mosaic = Mosaic(config_file = args.client_config)
 
-  # Open an api client project object for the defined project
-  project = api_mosaic.get_project(args.project_id)
-
-  # Get the role_id for the user
-  role_id = False
-  for user in project.get_roles()['data']:
-    if int(user['user_id']) == int(args.user_id):
-      role_id = user['id']
-      role_type_id = user['role_type_id']
-
-  # If the user wasn't found, fail
-  if not role_id:
-    fail('did not find user with id ' + str(args.user_id) + ' in this project')
-
-  # Update the role for this project
-  policy_ids = args.policy_ids.split(',') if ',' in args.policy_ids else [args.policy_ids]
-  project.put_project_role(role_id, role_type_id, user_id = args.user_id, policy_ids = policy_ids)
+  # Update the project roles
+  project.put_project_role(role_id, role_type_id, user_id=None, can_download=None, can_launch_app=None, policy_ids=None)
+  data = api_mosaic.post_attribute_form(name = args.name, attributes = attributes_json)
 
 # Input options
 def parse_command_line():
@@ -51,14 +37,13 @@ def parse_command_line():
   parser.add_argument('--client_config', '-c', required = True, metavar = 'string', help = 'The ini config file for Mosaic')
   parser.add_argument('--api_client', '-a', required = False, metavar = 'string', help = 'The api_client directory')
 
-  # The project id to which the filter is to be added is required
-  parser.add_argument('--project_id', '-p', required = True, metavar = 'integer', help = 'The Mosaic project id to upload attributes to')
+  # Required arguments
+  parser.add_argument('--name', '-n', required = True, metavar = 'string', help = 'The name of the attribute form')
 
-  # User ids
-  parser.add_argument('--user_id', '-u', required = True, metavar = 'integer', help = 'The user id to assign the policy to')
-
-  # Policy ids
-  parser.add_argument('--policy_ids', '-o', required = True, metavar = 'string', help = 'A comma separated list of policy ids to assign to this user')
+  # Optional arguments
+  parser.add_argument('--required_attributes', '-r', required = False, metavar = 'string', help = 'A comma separated list of the ids of all required attributes')
+  parser.add_argument('--suggested_attributes', '-s', required = False, metavar = 'string', help = 'A comma separated list of the ids of all suggested attributes')
+  parser.add_argument('--optional_attributes', '-o', required = False, metavar = 'string', help = 'A comma separated list of the ids of all optional attributes')
 
   return parser.parse_args()
 
