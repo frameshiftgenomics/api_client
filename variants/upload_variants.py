4b86fa31a7ed12cbb05b49b596cefcd82951db2c
Alistair Ward <u0991467@redwood5.ipoib.int.chpc.utah.edu>
2025-06-24 09:50:36 -0600

    1. Added check to variant upload that the vcf_sample_name is set for vcf attached to project

--- variants/upload_variants.py
+++ variants/upload_variants.py
@@ -31,36 +31,46 @@ def main():
   for sample in project.get_samples():
     for sample_file in project.get_sample_files(sample['id']):
       if sample_file['type'] == 'vcf':
+        if not sample_file['vcf_sample_name']:
+          fail('Vcf file attached to the project does not have the vcf_sample_field set. This is required for the upload to complete')
         has_vcfs = True
         break
   if not has_vcfs and not args.sample_map:
     fail('project has no associated vcf files and so a sample map (--sample_map, -s) is required')
 
+  # Set the sample map and notifications
   sample_map = args.sample_map if args.sample_map else None
+  notifications = 'false' if args.enable_notifications else 'true'
 
   # Upload the variants
-  notifications = 'false' if args.enable_notifications else 'true'
-  data = project.post_variant_file(args.vcf, upload_type = args.method, disable_successful_notification = notifications, sample_map=sample_map)
-  print(data['message'], '. Variant upload job id: ', data['redis_job_id'], sep = '')
+  try:
+    data = project.post_variant_file(args.vcf, upload_type = args.method, disable_successful_notification = notifications, sample_map=sample_map)
+  except Exception as e:
+    fail('Failed to upload variants. Error: ' + str(e))
 
 # Input options
 def parse_command_line():
   parser = argparse.ArgumentParser(description='Process the command line arguments')
+  api_arguments = parser.add_argument_group('API Arguments')
+  project_arguments = parser.add_argument_group('Project Arguments')
+  required_arguments = parser.add_argument_group('Required Arguments')
+  optional_arguments = parser.add_argument_group('Optional Arguments')
+  display_arguments = parser.add_argument_group('Display Information')
 
   # Define the location of the api_client and the ini config file
-  parser.add_argument('--client_config', '-c', required = True, metavar = 'string', help = 'The ini config file for Mosaic')
-  parser.add_argument('--api_client', '-a', required = False, metavar = 'string', help = 'The api_client directory')
+  api_arguments.add_argument('--client_config', '-c', required = True, metavar = 'string', help = 'The ini config file for Mosaic')
+  api_arguments.add_argument('--api_client', '-a', required = False, metavar = 'string', help = 'The api_client directory')
 
   # The project id to which the filter is to be added is required
-  parser.add_argument('--project_id', '-p', required = True, metavar = 'integer', help = 'The Mosaic project id to add variant filters to')
+  project_arguments.add_argument('--project_id', '-p', required = True, metavar = 'integer', help = 'The Mosaic project id to add variant filters to')
 
   # Additional arguments
-  parser.add_argument('--method', '-m', required = True, metavar = 'string', help = 'The variant upload method: "allele, no-validation, position, raw, sv-no-validation"')
-  parser.add_argument('--vcf', '-v', required = True, metavar = 'string', help = 'The vcf file to upload variants from')
-  parser.add_argument('--enable_notifications', '-e', required = False, action = 'store_true', help = 'If set, notifications will be provided. Otherwise, notifications will only be provided for failures')
+  required_arguments.add_argument('--method', '-m', required = True, metavar = 'string', help = 'The variant upload method: "allele, no-validation, position, raw, sv-no-validation"')
+  required_arguments.add_argument('--vcf', '-v', required = True, metavar = 'string', help = 'The vcf file to upload variants from')
+  optional_arguments.add_argument('--enable_notifications', '-e', required = False, action = 'store_true', help = 'If set, notifications will be provided. Otherwise, notifications will only be provided for failures')
 
   # If there is no vcf file attached to the project, we need a file connecting the sample ids to the vcf sample names
-  parser.add_argument('--sample_map', '-s', required = False, metavar = 'string', help = 'The sample map file which is required if there are no vcf files attached to the project')
+  optional_arguments.add_argument('--sample_map', '-s', required = False, metavar = 'string', help = 'The sample map file which is required if there are no vcf files attached to the project')
 
   return parser.parse_args()
 
