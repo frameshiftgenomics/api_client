30b6b4ec6fa2e00c77c6134393dc8a5a3410eb21
Alistair Ward <u0991467@redwood5.ipoib.int.chpc.utah.edu>
2025-07-17 06:19:39 -0600

    1. Added a samples/get_relation.py route to get the name and id of a sample based on the relation sample attribute

---
+++ samples/get_relation.py
@@ -0,0 +1,82 @@
+import os
+import argparse
+
+from pprint import pprint
+from sys import path
+
+def main():
+
+  # Parse the command line
+  args = parse_command_line()
+
+  # If the api_client path was not specified, get it from the script path
+  if not args.api_client:
+    try:
+      args.api_client = os.path.dirname(os.path.realpath(__file__)).split('api_client')[0] + str('api_client')
+    except:
+      fail('Could not get the api_client path from the command. Please specify using --api_client / -a')
+
+  # Import the api client
+  path.append(args.api_client)
+  try:
+    from mosaic import Mosaic, Project, Store
+  except:
+    fail('Cannot find mosaic. Please set the --api_client / -a argument')
+  api_store = Store(config_file = args.client_config)
+  api_mosaic = Mosaic(config_file = args.client_config)
+
+  # Open an api client project object for the defined project
+  project = api_mosaic.get_project(args.project_id)
+
+  # Get the sample attributes and find the id of the Relation attribute
+  relation_values = None
+  for attribute in project.get_sample_attributes(include_values = 'true'):
+    if attribute['name'] == 'Relation':
+      relation_values = {}
+      for value in attribute['values']:
+        relation_values[value['sample_id']] = value['value']
+      break
+  if not relation_values:
+    fail('The Relation sample attribute could not be found in the project')
+
+  # Delete the file
+  samples = project.get_samples()
+  for sample in samples:
+    if args.relation:
+      if relation_values[sample['id']] == args.relation:
+        print(sample['name'], sample['id'], relation_values[sample['id']], sep = '\t')
+
+    else:
+      print(sample['name'], sample['id'], relation_values[sample['id']], sep = '\t')
+
+# Input options
+def parse_command_line():
+  parser = argparse.ArgumentParser(description='Process the command line arguments')
+  api_arguments = parser.add_argument_group('API Arguments')
+  project_arguments = parser.add_argument_group('Project Arguments')
+  required_arguments = parser.add_argument_group('Required Arguments')
+  optional_arguments = parser.add_argument_group('Optional Arguments')
+  display_arguments = parser.add_argument_group('Display Information')
+
+  # Define the location of the api_client and the ini config file
+  api_arguments.add_argument('--client_config', '-c', required = True, metavar = 'string', help = 'The ini config file for Mosaic')
+  api_arguments.add_argument('--api_client', '-a', required = False, metavar = 'string', help = 'The api_client directory')
+
+  # The project id to which the filter is to be added is required
+  project_arguments.add_argument('--project_id', '-p', required = True, metavar = 'integer', help = 'The Mosaic project id to upload attributes to')
+
+  # Which relation to look for
+  optional_arguments.add_argument('--relation', '-r', required = False, metavar = 'string', help = 'The name of the relation to find, e.g. proband, or mother. If not set, all samples will be returned')
+
+  # Output ids only
+  display_arguments.add_argument('--ids_only', '-d', required = False, action = 'store_true', help = 'Only output sample ids')
+
+  return parser.parse_args()
+
+# If the script fails, provide an error message and exit
+def fail(message):
+  print('ERROR: ', message, sep = '')
+  exit(1)
+
+if __name__ == "__main__":
+  main()
