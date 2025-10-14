216a28a91ab250d8dbc8f0b1c939d8c13fdb470c
Alistair Ward <u0991467@redwood5.ipoib.int.chpc.utah.edu>
2025-10-14 10:31:09 -0600

    1. Minor updates

--- super_admin/get_user_info.py
+++ super_admin/get_user_info.py
@@ -30,21 +30,27 @@ def main():
 
   # Get the user info
   user_data = api_mosaic.get_user_info(args.user_id)
-  print(user_data['id'], ': ', sep = '')
-  print('  email: ', user_data['email'], sep = '')
-  print('  first name: ', user_data['first_name'], sep = '')
-  print('  last name: ', user_data['last_name'], sep = '')
-  print('  username: ', user_data['username'], sep = '')
-  print('  CAS username: ', user_data['cas_username'], sep = '')
 
   # Format the time stringd
   format_string = "%Y-%m-%dT%H:%M:%S.%fZ"
   created_at = str(datetime.strptime(user_data['created_at'], format_string)).split('.')[0]
   last_login_at = str(datetime.strptime(user_data['last_login_at'], format_string)).split('.')[0]
 
-  print('  created: ', created_at, sep = '')
-  print('  confirmation status: ', user_data['confirmation_status'], sep = '')
-  print('  last login: ', last_login_at, sep = '')
+  # Write out the information
+  if args.last_login:
+    print(last_login_at)
+
+  # Write all data
+  else:
+    print(user_data['id'], ': ', sep = '')
+    print('  email: ', user_data['email'], sep = '')
+    print('  first name: ', user_data['first_name'], sep = '')
+    print('  last name: ', user_data['last_name'], sep = '')
+    print('  username: ', user_data['username'], sep = '')
+    print('  CAS username: ', user_data['cas_username'], sep = '')
+    print('  created: ', created_at, sep = '')
+    print('  confirmation status: ', user_data['confirmation_status'], sep = '')
+    print('  last login: ', last_login_at, sep = '')
 
 # Input options
 def parse_command_line():
@@ -61,6 +67,9 @@ def parse_command_line():
 
   # The user id
   required_arguments.add_argument('--user_id', '-i', required = True, metavar = 'integer', help = 'The user id')
+
+  # Optional display arguments
+  display_arguments.add_argument('--last_login', '-l', required = False, action = 'store_true', help = 'If set, only the last login date will be output for the user')
 
   return parser.parse_args()
 
--- variants/get_watchlist.py
+++ variants/get_watchlist.py
@@ -26,31 +26,51 @@ def main():
   api_mosaic = Mosaic(config_file = args.client_config)
 
   # Open an api client project object for the defined project
-  project = api_mosaic.get_project(args.project_id)
+  try:
+    project = api_mosaic.get_project(args.project_id)
+  except Exception as e:
+    fail('Failed to open project. Error was: ' + str(e))
 
   # Get the variant sets in the project
-  data = project.get_variant_watchlist(args.include_variant_data)
+  include_variant_data = 'true' if args.include_variant_data else 'false'
+  try:
+    data = project.get_variant_watchlist(include_variant_data = include_variant_data)
+  except Exception as e:
+    fail('Failed to get variant watchlist. Error was: ' + str(e))
+
+  # If only the number of variants is required
+  if args.output_number_variants:
+    print(len(data['variant_ids']))
 
   # Format the output based on whether variants were output
-  print(data['name'], ': ', data['id'], sep = '')
-  print('  description: ', data['description'], sep = '')
-  print('  is_watchlist: ', data['is_watchlist'], sep = '')
-  print('  is_public_to_project: ', data['is_public_to_project'], sep = '')
-  print('  Number of variants: ', len(data['variant_ids']), sep = '')
+  else:
+    print(data['name'], ': ', data['id'], sep = '')
+    print('  description: ', data['description'], sep = '')
+    print('  is_watchlist: ', data['is_watchlist'], sep = '')
+    print('  is_public_to_project: ', data['is_public_to_project'], sep = '')
+    print('  Number of variants: ', len(data['variant_ids']), sep = '')
 
 # Input options
 def parse_command_line():
   parser = argparse.ArgumentParser(description='Process the command line arguments')
+  api_arguments = parser.add_argument_group('API Arguments')
+  project_arguments = parser.add_argument_group('Project Arguments')
+  required_arguments = parser.add_argument_group('Required Arguments')
+  optional_arguments = parser.add_argument_group('Optional Arguments')
+  display_arguments = parser.add_argument_group('Display Information')
 
   # Define the location of the api_client and the ini config file
-  parser.add_argument('--client_config', '-c', required = True, metavar = 'string', help = 'The ini config file for Mosaic')
-  parser.add_argument('--api_client', '-a', required = False, metavar = 'string', help = 'The api_client directory')
+  api_arguments.add_argument('--client_config', '-c', required = True, metavar = 'string', help = 'The ini config file for Mosaic')
+  api_arguments.add_argument('--api_client', '-a', required = False, metavar = 'string', help = 'The api_client directory')
 
   # The project id to which the filter is to be added is required
-  parser.add_argument('--project_id', '-p', required = True, metavar = 'integer', help = 'The Mosaic project id to add variant filters to')
+  project_arguments.add_argument('--project_id', '-p', required = True, metavar = 'integer', help = 'The Mosaic project id to add variant filters to')
 
   # Should variant data be returned
-  parser.add_argument('--include_variant_data', '-i', required = False, action = 'store_true', help = 'Should variant data be returned')
+  optional_arguments.add_argument('--include_variant_data', '-i', required = False, action = 'store_true', help = 'Should variant data be returned')
+
+  # Display options
+  display_arguments.add_argument('--output_number_variants', '-nv', required = False, action = 'store_true', help = 'Only output the number of variants in the watchlist')
 
   return parser.parse_args()
 
