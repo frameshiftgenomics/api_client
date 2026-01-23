815fec247fed4d8e1d45337d2c6f4153020676e5
Alistair Ward <u0991467@redwood5.ipoib.int.chpc.utah.edu>
2026-01-23 14:48:50 -0700

    1. Added a script to get the id of a project from it's name

---
+++ projects/get_project_id_by_name.py
@@ -0,0 +1,69 @@
+import os
+import argparse
+
+from sys import path
+from pprint import pprint
+
+def main():
+  global allowed_references
+  global system_projects
+
+  # Parse the command line
+  args = parse_command_line()
+
+  # If the api_client path was not specified, get it from the script path
+  if not args.api_client:
+    try:
+      args.api_client = os.path.dirname(os.path.realpath(__file__)).split('api_client')[0] + str('api_client')
+    except:
+      fail('Could not get the api_client path from the command. Please specify using --api_client / -a')
+
+  # Import the api client
+  path.append(args.api_client)
+  try:
+    from mosaic import Mosaic, Project, Store
+  except:
+    fail('Cannot find mosaic. Please set the --api_client / -a argument')
+  api_store = Store(config_file = args.client_config)
+  api_mosaic = Mosaic(config_file = args.client_config)
+
+  # Get all the available projects
+  project_id = False
+  for project_info in api_mosaic.get_projects():
+
+    # Check if the project name is the same as defined on the command line
+    if str(project_info['name']) == str(args.project_name):
+      project_id = project_info['id']
+      break
+
+  # Write out the project id
+  if not project_id:
+    print('No project found with the given name')
+  else:
+    print(project_id)
+
+# Input options
+def parse_command_line():
+  parser = argparse.ArgumentParser(description='Process the command line arguments')
+  api_arguments = parser.add_argument_group('API Arguments')
+  project_arguments = parser.add_argument_group('Project Arguments')
+  required_arguments = parser.add_argument_group('Required Arguments')
+  optional_arguments = parser.add_argument_group('Optional Arguments')
+  display_arguments = parser.add_argument_group('Display Information')
+
+  # Define the location of the api_client and the ini config file
+  api_arguments.add_argument('--client_config', '-c', required = True, metavar = 'string', help = 'The ini config file for Mosaic')
+  api_arguments.add_argument('--api_client', '-a', required = False, metavar = 'string', help = 'The api_client directory')
+
+  # Get the name of the project to get the id for
+  required_arguments.add_argument('--project_name', '-p', required = True, metavar = 'string', help = 'The name of the project to find the id of')
+
+  return parser.parse_args()
+
+# If the script fails, provide an error message and exit
+def fail(message):
+  print('ERROR: ', message, sep = '')
+  exit(1)
+
+if __name__ == "__main__":
+  main()
