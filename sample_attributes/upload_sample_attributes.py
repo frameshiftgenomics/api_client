7fefb433faef673c13b8fe29d6d80b7228efe985
Alistair Ward <u0991467@redwood5.ipoib.int.chpc.utah.edu>
2026-02-13 11:18:52 -0700

    1. Added sample attributes file upload

---
+++ sample_attributes/upload_sample_attributes.py
@@ -0,0 +1,67 @@
+import os
+import argparse
+
+from pprint import pprint
+from sys import path
+
+def main():
+
+  # Parse the command line
+  args = parse_command_line()
+
+  # If the api_client path was not specified, get it from the script path
+  if not args.api_client:
+    try:
+      args.api_client = os.path.dirname(os.path.realpath(__file__)).split('api_client')[0] + str('api_client')
+    except:
+      fail('Could not get the api_client path from the command. Please specify using --api_client / -a')
+
+  # Import the api client
+  path.append(args.api_client)
+  try:
+    from mosaic import Mosaic, Project, Store
+  except:
+    fail('Cannot find mosaic. Please set the --api_client / -a argument')
+  api_store  = Store(config_file = args.client_config)
+  api_mosaic = Mosaic(config_file = args.client_config)
+
+  # Otherwise open this project ready for annotation upload
+  try:
+    project = api_mosaic.get_project(upload_project_id)
+  except Exception as e:
+    fail('failed to open project. Error was: ' + str(e))
+
+  # Upload the sample attributes file
+  try:
+    project.post_upload_sample_attributes(args.tsv)
+  except Exception as e:
+    fail('Failed to upload sample attributes. Error was: ' + str(e))
+
+# Input options
+def parse_command_line():
+  parser = argparse.ArgumentParser(description='Process the command line arguments')
+  api_arguments = parser.add_argument_group('API Arguments')
+  project_arguments = parser.add_argument_group('Project Arguments')
+  required_arguments = parser.add_argument_group('Required Arguments')
+  optional_arguments = parser.add_argument_group('Optional Arguments')
+  display_arguments = parser.add_argument_group('Display Information')
+
+  # Define the location of the api_client and the ini config file
+  api_arguments.add_argument('--client_config', '-c', required = True, metavar = 'string', help = 'The ini config file for Mosaic')
+  api_arguments.add_argument('--api_client', '-a', required = False, metavar = 'string', help = 'The api_client directory')
+
+  # The project id
+  parser.add_argument('--project_id', '-p', required = False, metavar = 'integer', help = 'The Mosaic project id to upload annotations to. Only necessary for private or custom annotations')
+
+  # Additional arguments
+  required_arguments.add_argument('--tsv', '-t', required = True, metavar = 'string', help = 'The annotation tsv file to upload')
+
+  return parser.parse_args()
+
+# If the script fails, provide an error message and exit
+def fail(message):
+  print('ERROR: ', message, sep = '')
+  exit(1)
+
+if __name__ == "__main__":
+  main()
