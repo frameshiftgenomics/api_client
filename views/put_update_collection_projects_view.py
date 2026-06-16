f9d043e419f2ec675668bb028f9f257c7bf303ae
Alistair Ward <u0991467@redwood5.ipoib.int.chpc.utah.edu>
2026-06-16 08:27:23 -0600

    1. Updated views commands

--- views/put_update_collection_projects_view.py
+++ views/put_update_collection_projects_view.py
@@ -45,16 +45,18 @@ def main():
       project_attribute_ids.append(attribute_info['id'])
 
   # Loop over the list of attribute ids and ensure they exist in the project
-  attribute_ids = args.attribute_ids.split(',') if ',' in args.attribute_ids else [args.attribute_ids]
-  missing_ids = ''
-  for attribute_id in attribute_ids:
-    if int(attribute_id) not in project_attribute_ids:
-      missing_ids += attribute_id + ','
-  missing_ids = missing_ids.rstrip(',')
-  if len(missing_ids) > 0:
-    print('The following attribute ids are not in the selected project and so cannot be part of a view:')
-    print('  ', missing_ids)
-    exit(0)
+  attribute_ids = []
+  if args.attribute_ids:
+    attribute_ids = args.attribute_ids.split(',') if ',' in args.attribute_ids else [args.attribute_ids]
+    missing_ids = ''
+    for attribute_id in attribute_ids:
+      if int(attribute_id) not in project_attribute_ids:
+        missing_ids += attribute_id + ','
+    missing_ids = missing_ids.rstrip(',')
+    if len(missing_ids) > 0:
+      print('The following attribute ids are not in the selected project and so cannot be part of a view:')
+      print('  ', missing_ids)
+      exit(0)
 
   # Set the description
   description = args.description if args.description else None
@@ -68,9 +70,9 @@ def main():
   attribute_filters = args.attribute_filters if args.attribute_filters else None
   pagination = args.pagination if args.pagination else None
 
-  # POST the new view
+  # Update the view
   try:
-    project.post_create_collection_projects_view(args.name, description = description, icon = icon, selected_attribute_ids = attribute_ids, attribute_filters = attribute_filters, pagination = pagination)
+    project.put_update_collection_projects_view(args.name, description = description, icon = icon, selected_attribute_ids = attribute_ids, attribute_filters = attribute_filters, pagination = pagination)
   except Exception as e:
     fail('failed to POST new data group view. Error wes: ' + str(e))
 
@@ -91,10 +93,11 @@ def parse_command_line():
   project_arguments.add_argument('--project_id', '-p', required = True, metavar = 'integer', help = 'The Mosaic project id to upload attributes to')
 
   # Required arguments
-  required_arguments.add_argument('--name', '-n', required = True, metavar = 'string', help = 'The name of the new data group view')
-  required_arguments.add_argument('--attribute_ids', '-ai', required = True, metavar = 'string', help = 'A comma separated list of attribute ids to appear in the view')
+  required_arguments.add_argument('--view_id', '-v', required = True, metavar = 'integer', help = 'The id of the view to update')
 
   # Optional arguments
+  optional_arguments.add_argument('--name', '-n', required = False, metavar = 'string', help = 'The name of the new data group view')
+  optional_arguments.add_argument('--attribute_ids', '-ai', required = False, metavar = 'string', help = 'A comma separated list of attribute ids to appear in the view')
   optional_arguments.add_argument('--attribute_filters', '-af', required = False, metavar = 'string', help = 'Attribute filters. This is a json object of the form: [{"uid": "attribute_uid", "value_type": "string", "values": ["VALUE"]}]')
   optional_arguments.add_argument('--pagination', '-pg', required = False, metavar = 'string', help = 'Paginsation settings. This is a json object of the form: {"descending": "false", "page": 1, "rowsPerPage": 10, "sortBy": "attribute_uid"}')
   optional_arguments.add_argument('--description', '-d', required = False, metavar = 'string', help = 'A description of the new data group view')
